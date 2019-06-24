2340eacf53c08d92342aed4831963d073b0f7072
Brian Corbett <briancorbett@brighthouse.com>
2019-06-24 19:39:08 -0400

    function for concert this work

--- liri.js
+++ liri.js
@@ -6,28 +6,23 @@ var spotify = new Spotify(keys.spotify);
 
 var axios = require("axios")
 
-var moment = require('moment');
+var moment = require("moment");
 
+var Spotify = require("node-spotify-api");
 
+// input
+var query = process.argv;
+var type =process.argv[2];
+var array = [];
 
-// variables to call functions
-var spotifyThisSong = function() {
-    console.log (""+ input);
+//loop through 
+for (var i = 3; i < query.length; i++) {
+    array.push(query[i]);
+    array.push("+");
+}
 
-};
-
-var movieThis = function () {
-    console.log ("" + input);
-};
-
-var doWhatItSays = function(){
-    console.log ("" + input);
-
-};
-
-var concertThis = function() {
-    console.log ();
-};
+array.splice(-1);
+var finalSearch = array.join("");
 
 // Switch statement to call functions
 
@@ -49,4 +44,48 @@ switch (command) {
         break;
 
 }
+
+
+
+
+// variables to call functions
+var spotifyThisSong = function() {
+    if (finaSearch === ""){
+        console.log ("\n");
+        console.log("No Artist entered");
+        console.log("\n");
+
+    }
+    else {
+        axios.get("https://rest.bandsintown.com/artists/" + finalSearch + "/events?app_id=codingbootcamp").then(
+            function (response){
+             if (response.data.length <= 0){
+                 console.log("no info for this artist");
+             }  
+             else {
+                 
+                 }
+             } 
+                
+            
+        
+    
+
+            }
+
+var movieThis = function () {
+    console.log ("" + input);
+};
+
+var doWhatItSays = function(){
+    console.log ("" + input);
+
+};
+
+var concertThis = function() {
+    console.log ();
+};
+
+// Switch statement to call functions
+
 
